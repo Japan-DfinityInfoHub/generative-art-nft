106d885008ce4e06e35971a3c1e88e9d29ebe254
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-05-26 08:34:55 +0900

    add error handling to invalid args

--- src/uploadImages/index.ts
+++ src/uploadImages/index.ts
@@ -3,6 +3,10 @@ import { upload } from './upload';
 const startIndex = parseInt(process.argv[2]);
 const endIndex = parseInt(process.argv[3]);
 
+if (isNaN(startIndex) || isNaN(endIndex)) {
+  throw 'You must specify two args as type of number.';
+}
+
 const imagesSaveDir = './dist';
 
 upload(startIndex, endIndex, imagesSaveDir);
